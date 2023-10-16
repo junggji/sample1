b8b45b265d06f155a87189421c01f139639371b2
junggji <calrinar@gmail.com>
2023-10-16 16:33:56 +0900

    todo app

--- src/main/java/com/example/sample1/dao/TodoDao.java
+++ src/main/java/com/example/sample1/dao/TodoDao.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,15 +15,14 @@ public class TodoDao {
     @Autowired
     private DataSource dataSource;
 
-
-    public List<Todo> list() throws Exception{
+    public List<Todo> list() throws Exception {
         String sql = "SELECT * FROM todo ORDER BY id DESC";
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
 
         List<Todo> list = new ArrayList<>();
-        try (connection;statement;resultSet) {
+        try (connection; statement; resultSet) {
             while (resultSet.next()) {
                 Todo todo = new Todo();
                 todo.setId(resultSet.getInt("id"));
@@ -34,9 +31,27 @@ public class TodoDao {
 
                 list.add(todo);
             }
+
         }
+
+        return list;
     }
 
-    public void insert(Todo todo) {
+    public boolean insert(Todo todo) throws SQLException {
+        String sql = """
+                INSERT INTO todo (todo)
+                VALUE (?)            
+                """;
+
+        Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        try (connection; statement) {
+            statement.setString(1, todo.getTodo());
+            int rows = statement.executeUpdate();
+
+            return rows == 1;
+        }
+
     }
 }
